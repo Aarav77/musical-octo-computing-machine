b3c6ee04e0738178fdeb56a1ed8d638cc0549efe
Aarav77 <64909432+Aarav77@users.noreply.github.com>
2021-04-29 17:11:22 +0530

    Add files via upload

--- c110/sampling.py
+++ c110/sampling.py
@@ -6,8 +6,8 @@ import pandas as pd
 import csv
 
 def random_set_of_means(counter):
-    datafile = pd.read_csv("data.csv")
-    data =datafile["average"].tolist()
+    datafile = pd.read_csv("medium_data.csv")
+    data =datafile["reading_time"].tolist()
     dataset = []
     for i in range (0, counter):
         random_index = random.randint(0,len(data)-1)
@@ -19,8 +19,8 @@ def random_set_of_means(counter):
 
 def show_fig(mean_list):
     datafile= mean_list
-    fig = ff.create_distplot([datafile], ["average"], show_hist=False)
-    fig.add_trace(go.Scatter(x=["average", "average"], y=[0, 12], mode="lines", name="MEAN"))
+    fig = ff.create_distplot([datafile], ["reading_time"], show_hist=False)
+    fig.add_trace(go.Scatter(x=["reading_time", "reading_time"], y=[0, 12], mode="lines", name="MEAN"))
     fig.show()
 
 def setup():
